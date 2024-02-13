294e086533e5235f4810c10601892aa64b3ef18a
Sam Royall <samrroyall@gmail.com>
2024-02-12 19:09:05 -0600

    [#32] :: Move TreeNode inspection logic to getChildren() method and add more tests (#33)

--- src/services/solver.rs
+++ src/services/solver.rs
@@ -1,22 +1,21 @@
+use std::cell::RefCell;
 use std::collections::{HashSet, VecDeque};
 use std::rc::Rc;
 
 use crate::errors::board::Error as BoardError;
 use crate::models::game::{board::Board, moves::FlatBoardMove};
 
+type Edge = Rc<RefCell<TreeNode>>;
+
 #[derive(Debug, Clone)]
 struct TreeNode {
-    parent: Option<Rc<Self>>,
-    move_: Option<FlatBoardMove>,
-    board: Option<Board>,
+    parent: Option<Edge>,
+    move_: FlatBoardMove,
+    board: Board,
 }
 
 impl TreeNode {
-    pub fn new(
-        parent: Option<Rc<Self>>,
-        move_: Option<FlatBoardMove>,
-        board: Option<Board>,
-    ) -> Self {
+    pub fn new(parent: Option<Edge>, move_: FlatBoardMove, board: Board) -> Self {
         Self {
             parent,
             move_,
@@ -24,36 +23,12 @@ impl TreeNode {
         }
     }
 
-    pub fn move_(&self) -> Option<&FlatBoardMove> {
-        self.move_.as_ref()
-    }
-
-    pub fn board(&self) -> Option<&Board> {
-        self.board.as_ref()
-    }
-
-    pub fn parent(&self) -> Option<Rc<Self>> {
-        self.parent.as_ref().map(Rc::clone)
-    }
-
-    pub fn collect(&self) -> Vec<FlatBoardMove> {
-        let mut moves = vec![];
-
-        let mut curr_node = Some(Rc::new(self.clone()));
-
-        while let Some(node) = curr_node.as_ref() {
-            if let Some(move_) = node.move_() {
-                moves.push(move_.clone());
-
-                curr_node = node.parent();
-
-                continue;
-            }
-
-            break;
+    pub fn from_board(board: Board) -> Self {
+        Self {
+            parent: None,
+            move_: FlatBoardMove::default(),
+            board,
         }
-
-        moves.into_iter().rev().collect()
     }
 }
 
@@ -63,23 +38,42 @@ pub struct Solver {
 }
 
 impl Solver {
-    fn get_children(board: &Board, parent_node: &Rc<TreeNode>) -> Vec<TreeNode> {
+    fn upsert_hash(&mut self, board_hash: String) -> bool {
+        if self.seen.contains(&board_hash) {
+            false
+        } else {
+            self.seen.insert(board_hash);
+            true
+        }
+    }
+
+    fn get_children(&mut self, parent_node: &Rc<RefCell<TreeNode>>) -> Vec<TreeNode> {
         let mut children = vec![];
+
+        let board = parent_node.borrow().board.clone();
 
         for (block_idx, moves) in board.get_next_moves().into_iter().enumerate() {
             for move_ in moves {
-                let board_move = FlatBoardMove::new(block_idx, &move_);
+                let child_move = FlatBoardMove::new(block_idx, &move_);
 
-                if let Some(parent_move) = parent_node.move_() {
-                    if board_move.is_opposite(parent_move) {
-                        continue;
-                    }
+                if child_move.is_opposite(&parent_node.borrow().move_) {
+                    continue;
+                }
+
+                let mut child_board = board.clone();
+
+                child_board
+                    .move_block_optimistic(block_idx, move_.row_diff, move_.col_diff)
+                    .unwrap();
+
+                if !self.upsert_hash(child_board.hash()) {
+                    continue;
                 }
 
                 children.push(TreeNode::new(
-                    Some(Rc::clone(parent_node)),
-                    Some(board_move),
-                    Some(board.clone()),
+                    Some(parent_node.clone()),
+                    child_move,
+                    child_board,
                 ));
             }
         }
@@ -87,20 +81,12 @@ impl Solver {
         children
     }
 
-    fn upsert_hash(&mut self, board_hash: String) -> bool {
-        if self.seen.contains(&board_hash) {
-            return false;
-        }
+    fn bfs(&mut self, root: TreeNode) -> Option<Rc<RefCell<TreeNode>>> {
+        let root_cell = Rc::new(RefCell::new(root));
 
-        self.seen.insert(board_hash);
+        self.seen.insert(root_cell.borrow().board.hash());
 
-        true
-    }
-
-    fn bfs(&mut self, root: Rc<TreeNode>) -> Option<Rc<TreeNode>> {
-        self.seen.insert(root.board().unwrap().hash());
-
-        let mut queue = VecDeque::from([root]);
+        let mut queue = VecDeque::from([root_cell]);
 
         while !queue.is_empty() {
             let queue_size = queue.len();
@@ -108,24 +94,12 @@ impl Solver {
             for _ in 0..queue_size {
                 let node = queue.pop_front().unwrap();
 
-                let mut board = node.board().take().unwrap().to_owned();
-
-                if let Some(move_) = node.move_() {
-                    board
-                        .move_block_optimistic(move_.block_idx, move_.row_diff, move_.col_diff)
-                        .unwrap();
-
-                    if !self.upsert_hash(board.hash()) {
-                        continue;
-                    }
-
-                    if board.is_solved() {
-                        return Some(node.clone());
-                    }
+                if node.borrow().board.is_solved() {
+                    return Some(node.clone());
                 }
 
-                for child in Self::get_children(&board, &node) {
-                    queue.push_back(Rc::new(child));
+                for child in self.get_children(&node) {
+                    queue.push_back(Rc::new(RefCell::new(child)));
                 }
             }
         }
@@ -151,9 +125,25 @@ impl Solver {
     }
 
     pub fn solve(&mut self) -> Option<Vec<FlatBoardMove>> {
-        let root = TreeNode::new(None, None, Some(self.start_board.clone()));
+        let root_node = TreeNode::from_board(self.start_board.clone());
 
-        self.bfs(Rc::new(root)).map(|leaf| leaf.collect())
+        self.bfs(root_node).map(|tail_node| {
+            let mut moves = vec![];
+
+            let mut maybe_node = Some(tail_node);
+
+            while let Some(node) = maybe_node {
+                let mut node = node.borrow_mut();
+
+                if node.parent.is_some() {
+                    moves.push(node.move_.clone());
+                }
+
+                maybe_node = node.parent.take();
+            }
+
+            moves.into_iter().rev().collect()
+        })
     }
 }
 
@@ -192,7 +182,26 @@ mod tests {
         assert!(Solver::new(board).is_err());
     }
 
-    fn test_board(blocks: &[PositionedBlock], expected_moves: usize) {
+    fn test_solution_works(blocks: &[PositionedBlock]) {
+        let mut board = Board::default();
+
+        for block in blocks.iter() {
+            board.add_block(block.clone()).unwrap();
+        }
+
+        let mut solver = Solver::new(board.clone()).unwrap();
+        let moves = solver.solve().unwrap();
+
+        for move_ in moves.iter() {
+            board
+                .move_block_optimistic(move_.block_idx, move_.row_diff, move_.col_diff)
+                .unwrap();
+        }
+
+        assert!(board.is_solved());
+    }
+
+    fn test_board_is_optimal(blocks: &[PositionedBlock], expected_moves: usize) {
         let mut board = Board::default();
 
         for block in blocks.iter() {
@@ -200,16 +209,13 @@ mod tests {
         }
 
         let mut solver = Solver::new(board).unwrap();
+        let moves = solver.solve().unwrap();
 
-        let maybe_moves = solver.solve();
-
-        assert!(maybe_moves.is_some());
-
-        assert_eq!(maybe_moves.unwrap().len(), expected_moves);
+        assert_eq!(moves.len(), expected_moves);
     }
 
     #[test]
-    fn test_classic_board() {
+    fn test_classic_board_solution_works() {
         let blocks = [
             PositionedBlock::new(3, 0, 0).unwrap(),
             PositionedBlock::new(4, 0, 1).unwrap(),
@@ -223,11 +229,29 @@ mod tests {
             PositionedBlock::new(1, 4, 3).unwrap(),
         ];
 
-        test_board(&blocks, 81);
+        test_solution_works(&blocks);
     }
 
     #[test]
-    fn test_easy_board() {
+    fn test_classic_board_is_optimal() {
+        let blocks = [
+            PositionedBlock::new(3, 0, 0).unwrap(),
+            PositionedBlock::new(4, 0, 1).unwrap(),
+            PositionedBlock::new(3, 0, 3).unwrap(),
+            PositionedBlock::new(3, 2, 0).unwrap(),
+            PositionedBlock::new(2, 2, 1).unwrap(),
+            PositionedBlock::new(3, 2, 3).unwrap(),
+            PositionedBlock::new(1, 3, 1).unwrap(),
+            PositionedBlock::new(1, 3, 2).unwrap(),
+            PositionedBlock::new(1, 4, 0).unwrap(),
+            PositionedBlock::new(1, 4, 3).unwrap(),
+        ];
+
+        test_board_is_optimal(&blocks, 81);
+    }
+
+    #[test]
+    fn test_easy_board_solution_works() {
         let blocks = [
             PositionedBlock::new(1, 0, 0).unwrap(),
             PositionedBlock::new(4, 0, 1).unwrap(),
@@ -244,11 +268,32 @@ mod tests {
             PositionedBlock::new(1, 4, 3).unwrap(),
         ];
 
-        test_board(&blocks, 17);
+        test_solution_works(&blocks);
     }
 
     #[test]
-    fn test_medium_board() {
+    fn test_easy_board_is_optimal() {
+        let blocks = [
+            PositionedBlock::new(1, 0, 0).unwrap(),
+            PositionedBlock::new(4, 0, 1).unwrap(),
+            PositionedBlock::new(1, 0, 3).unwrap(),
+            PositionedBlock::new(1, 1, 0).unwrap(),
+            PositionedBlock::new(1, 1, 3).unwrap(),
+            PositionedBlock::new(3, 2, 0).unwrap(),
+            PositionedBlock::new(1, 2, 1).unwrap(),
+            PositionedBlock::new(1, 2, 2).unwrap(),
+            PositionedBlock::new(3, 2, 3).unwrap(),
+            PositionedBlock::new(1, 3, 1).unwrap(),
+            PositionedBlock::new(1, 3, 2).unwrap(),
+            PositionedBlock::new(1, 4, 0).unwrap(),
+            PositionedBlock::new(1, 4, 3).unwrap(),
+        ];
+
+        test_board_is_optimal(&blocks, 17);
+    }
+
+    #[test]
+    fn test_medium_board_solution_works() {
         let blocks = [
             PositionedBlock::new(1, 0, 0).unwrap(),
             PositionedBlock::new(4, 0, 1).unwrap(),
@@ -262,11 +307,29 @@ mod tests {
             PositionedBlock::new(2, 4, 1).unwrap(),
         ];
 
-        test_board(&blocks, 40);
+        test_solution_works(&blocks);
     }
 
     #[test]
-    fn test_hard_board() {
+    fn test_medium_board_is_optimal() {
+        let blocks = [
+            PositionedBlock::new(1, 0, 0).unwrap(),
+            PositionedBlock::new(4, 0, 1).unwrap(),
+            PositionedBlock::new(1, 0, 3).unwrap(),
+            PositionedBlock::new(1, 1, 0).unwrap(),
+            PositionedBlock::new(1, 1, 3).unwrap(),
+            PositionedBlock::new(3, 2, 0).unwrap(),
+            PositionedBlock::new(3, 2, 1).unwrap(),
+            PositionedBlock::new(2, 2, 2).unwrap(),
+            PositionedBlock::new(2, 3, 2).unwrap(),
+            PositionedBlock::new(2, 4, 1).unwrap(),
+        ];
+
+        test_board_is_optimal(&blocks, 40);
+    }
+
+    #[test]
+    fn test_hard_board_solution_works() {
         let blocks = [
             PositionedBlock::new(1, 0, 0).unwrap(),
             PositionedBlock::new(4, 0, 1).unwrap(),
@@ -280,6 +343,24 @@ mod tests {
             PositionedBlock::new(2, 4, 1).unwrap(),
         ];
 
-        test_board(&blocks, 120);
+        test_solution_works(&blocks);
+    }
+
+    #[test]
+    fn test_hard_board_is_optimal() {
+        let blocks = [
+            PositionedBlock::new(1, 0, 0).unwrap(),
+            PositionedBlock::new(4, 0, 1).unwrap(),
+            PositionedBlock::new(1, 0, 3).unwrap(),
+            PositionedBlock::new(3, 1, 0).unwrap(),
+            PositionedBlock::new(3, 1, 3).unwrap(),
+            PositionedBlock::new(2, 2, 1).unwrap(),
+            PositionedBlock::new(1, 3, 0).unwrap(),
+            PositionedBlock::new(1, 3, 3).unwrap(),
+            PositionedBlock::new(2, 3, 1).unwrap(),
+            PositionedBlock::new(2, 4, 1).unwrap(),
+        ];
+
+        test_board_is_optimal(&blocks, 120);
     }
 }
