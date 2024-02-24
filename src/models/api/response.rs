a121f864f2b10c8250e914009e06d9df3f294374
Sam Royall <samrroyall@gmail.com>
2024-02-24 15:53:58 -0600

    [#46] :: Update responses to return objects not strings

--- src/models/api/response.rs
+++ src/models/api/response.rs
@@ -1,37 +1,44 @@
 use serde::Serialize;
 
-use crate::models::{db::tables::SelectableBoard, game::moves::FlatBoardMove};
+use crate::models::{
+    db::tables::SelectableBoard,
+    game::{
+        blocks::Positioned as PositionedBlock,
+        board::{Board as Board_, State as BoardState},
+        moves::{FlatBoardMove, FlatMove},
+    },
+};
 
 #[derive(Debug, Serialize)]
 pub struct Board {
     id: i32,
-    state: String,
-    blocks: String,
-    grid: String,
-    next_moves: String,
+    state: BoardState,
+    blocks: Vec<PositionedBlock>,
+    grid: [[u8; Board_::COLS as usize]; Board_::ROWS as usize],
+    next_moves: Vec<Vec<FlatMove>>,
 }
 
 impl Board {
     pub fn new(board_record: &SelectableBoard) -> Self {
         Self {
             id: board_record.id,
-            state: board_record.state.clone(),
-            blocks: board_record.blocks.clone(),
-            grid: board_record.grid.clone(),
-            next_moves: board_record.next_moves.clone(),
+            state: serde_json::from_str(&board_record.state).unwrap(),
+            blocks: serde_json::from_str(&board_record.blocks).unwrap(),
+            grid: serde_json::from_str(&board_record.grid).unwrap(),
+            next_moves: serde_json::from_str(&board_record.next_moves).unwrap(),
         }
     }
 }
 
 #[derive(Debug, Serialize)]
 pub struct Solved {
-    moves: String,
+    moves: Vec<FlatBoardMove>,
 }
 
 impl Solved {
-    pub fn new(moves: &Vec<FlatBoardMove>) -> Self {
+    pub fn new(moves: &[FlatBoardMove]) -> Self {
         Self {
-            moves: serde_json::to_string(moves).unwrap(),
+            moves: moves.to_owned(),
         }
     }
 }
